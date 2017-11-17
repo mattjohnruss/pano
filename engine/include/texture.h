7bd2b5fc4674143b945112e8c67e82d5e9438f63
Matthew Russell <matthewjohnrussell@gmail.com>
2017-11-17 15:35:59 +0000

    Texture: Texture base class; add Texture3D; fix channels via swizzling for RED and RG formats; tidy up

--- engine/include/texture.h
+++ engine/include/texture.h
@@ -3,8 +3,39 @@
 #include <glad/glad.h>
 
 #include <string>
+#include <vector>
 
-class Texture2D
+// TODO keep track of which texture units are active and automatically use next
+// available one in use() (static member of Texture or singleton
+// TextureManager?)
+
+// maybe we want a stack?
+// pop a texture unit off the stack when activated; push back on when deactivated.
+// std::stack tex_unit_stack;
+//
+// ideally we will make Mesh (and Model?) use the full Texture class instead of
+// the minimal MeshTexture
+//
+// for(unsigned i = 0; i < n_tex_units; ++i)
+// {
+//     tex_unit_stack.push(<tex_unit_free_value>);
+// }
+
+class Texture
+{
+public:
+    Texture(GLuint id);
+    virtual ~Texture();
+
+    virtual void use(GLenum active_texture) const = 0;
+
+    const GLuint id() const;
+
+protected:
+    GLuint id_;
+};
+
+class Texture2D : public Texture
 {
 public:
     Texture2D(const char *image_path);
@@ -12,10 +43,17 @@ public:
 
     void use(GLenum active_texture) const;
 
-    const GLuint id() const;
-
     static GLuint texture_from_file(const char *image_path, const std::string &directory);
+};
 
-private:
-    GLuint id_;
+class Texture3D : public Texture
+{
+public:
+    Texture3D(const std::vector<std::string> &image_paths);
+    ~Texture3D();
+
+    void use(GLenum active_texture) const;
+
+    static GLuint texture_from_file(
+            const std::vector<std::string> &image_paths, const std::string &directory);
 };
--- engine/src/texture.cpp
+++ engine/src/texture.cpp
@@ -6,7 +6,21 @@
 #include <iostream>
 #include <algorithm>
 
-Texture2D::Texture2D(const char *image_path) : id_(texture_from_file(image_path, ""))
+Texture::Texture(GLuint id) : id_(id)
+{
+}
+
+Texture::~Texture()
+{
+    glDeleteTextures(1, &id_);
+}
+
+const GLuint Texture::id() const
+{
+    return id_;
+}
+
+Texture2D::Texture2D(const char *image_path) : Texture(texture_from_file(image_path, ""))
 {
 }
 
@@ -18,11 +32,6 @@ void Texture2D::use(GLenum active_texture) const
 {
     glActiveTexture(active_texture);
     glBindTexture(GL_TEXTURE_2D, id_);
-}
-
-const GLuint Texture2D::id() const
-{
-    return id_;
 }
 
 GLuint Texture2D::texture_from_file(const char *image_path, const std::string &directory)
@@ -50,21 +59,40 @@ GLuint Texture2D::texture_from_file(const char *image_path, const std::string &d
     // generate a texture and store the id
     glGenTextures(1, &id);
 
+    // image dimensions and number of components
+    int image_width;
+    int image_height;
+    int n_components;
+
     // attempt to load image located at image_path into image_data
-    int image_width, image_height, n_components;
     unsigned char *image_data =
         stbi_load(filename.c_str(), &image_width, &image_height, &n_components, 0);
 
     // if the load was successful
     if(image_data != 0)
     {
+        // texture format
         GLenum texture_format;
+
+        // bind the texture at id
+        glBindTexture(GL_TEXTURE_2D, id);
 
         // set the texture format based on the number of components in the loaded image
         switch(n_components)
         {
             case 1:
                 texture_format = GL_RED;
+                {
+                    GLint swizzle_mask[] = {GL_RED, GL_RED, GL_RED, GL_ONE};
+                    glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzle_mask);
+                }
+                break;
+            case 2:
+                texture_format = GL_RG;
+                {
+                    GLint swizzle_mask[] = {GL_RED, GL_RED, GL_RED, GL_GREEN};
+                    glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzle_mask);
+                }
                 break;
             case 3:
                 texture_format = GL_RGB;
@@ -72,10 +100,11 @@ GLuint Texture2D::texture_from_file(const char *image_path, const std::string &d
             case 4:
                 texture_format = GL_RGBA;
                 break;
+            default:
+                std::cerr << "Texture2D::texture_from_file - n_components = " << n_components << " unsupported\n";
+                std::cerr << "Attempting to use texture_format = GL_RGB\n";
+                texture_format = GL_RGB;
         }
-
-        // bind the texture at id
-        glBindTexture(GL_TEXTURE_2D, id);
 
         // TODO move these parameters into member functions
         // make the texture wrap in both directions
@@ -84,9 +113,9 @@ GLuint Texture2D::texture_from_file(const char *image_path, const std::string &d
 
         // set min and mag filters
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);   
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         // create the texture from the image data
         glTexImage2D(GL_TEXTURE_2D, 0, texture_format, image_width, image_height, 0,
@@ -101,12 +130,167 @@ GLuint Texture2D::texture_from_file(const char *image_path, const std::string &d
     }
     else
     {
-        std::cerr << "ERROR::TEXTURE::STBI_LOAD_FAILED\n";
+        std::cerr << "Texture2D::texture_from_file - stbi_load failed\n";
         //exit(1);
     }
 
     // free the image memory
     stbi_image_free(image_data);
+
+    // return the texture id
+    return id;
+}
+
+Texture3D::Texture3D(const std::vector<std::string> &image_paths) :
+    Texture(texture_from_file(image_paths, ""))
+{
+    std::cout << "Texture3D::Texture3D - id_ = " << id_ << '\n';
+}
+
+Texture3D::~Texture3D()
+{
+}
+
+void Texture3D::use(GLenum active_texture) const
+{
+    glActiveTexture(active_texture);
+    glBindTexture(GL_TEXTURE_3D, id_);
+}
+
+GLuint Texture3D::texture_from_file(
+        const std::vector<std::string> &image_paths, const std::string &directory)
+{
+    // declare the texture handle
+    GLuint id;
+
+    // generate a texture and store the id
+    glGenTextures(1, &id);
+
+    // get the number of images
+    unsigned n_image = image_paths.size();
+
+    // check if the image list is empty
+    if(n_image == 0)
+    {
+        std::cerr << "Texture3D::texture_from_file - vector of image paths is empty\n";
+
+        // is it ok to return a texture handle of 0?
+        // trying to load 0 textures is a bug anyway so yolo
+        return 0;
+    }
+
+    // vector of processed filenames
+    std::vector<std::string> filenames(n_image);
+
+    // bind the texture at id
+    glBindTexture(GL_TEXTURE_3D, id);
+
+    // loop over the images
+    for(unsigned i = 0; i < n_image; ++i)
+    {
+        if(directory == "")
+        {
+            filenames[i] = std::string(image_paths[i]);
+        }
+        else
+        {
+            filenames[i] = directory + "/" + std::string(image_paths[i]);
+        }
+
+        // replace all occurences of Windows style '\' with '/' in texture path
+        std::replace(filenames[i].begin(), filenames[i].end(), '\\', '/');
+
+        // output the (parsed) texture path
+        std::cout << "Texture3D::texture_from_file - "
+                  << "filenames[" << i << "] = " << filenames[i] << '\n';
+
+        // image dimensions and number of components
+        int image_width;
+        int image_height;
+        int n_components;
+
+        // texture format
+        GLenum texture_format;
+
+        // attempt to load image located at image_path into image_data
+        unsigned char *image_data =
+            stbi_load(filenames[i].c_str(), &image_width, &image_height, &n_components, 0);
+
+        // if the load was successful
+        if(image_data != 0)
+        {
+            // check the number of components in the first image only and allocate gl storage
+            // allows us to loop only once over textures and not store all image data at once
+            if(i == 0)
+            {
+                // set the texture format based on the number of components in the loaded image
+                switch(n_components)
+                {
+                    case 1:
+                            texture_format = GL_RED;
+                        {
+                            GLint swizzle_mask[] = {GL_RED, GL_RED, GL_RED, GL_ONE};
+                            glTexParameteriv(GL_TEXTURE_3D, GL_TEXTURE_SWIZZLE_RGBA, swizzle_mask);
+                        }
+                        break;
+                    case 2:
+                            texture_format = GL_RG;
+                        {
+                            GLint swizzle_mask[] = {GL_RED, GL_RED, GL_RED, GL_GREEN};
+                            glTexParameteriv(GL_TEXTURE_3D, GL_TEXTURE_SWIZZLE_RGBA, swizzle_mask);
+                        }
+                        break;
+                    case 3:
+                        texture_format = GL_RGB;
+                        break;
+                    case 4:
+                        texture_format = GL_RGBA;
+                        break;
+                    default:
+                        std::cerr << "Texture3D::texture_from_file - n_components = " << n_components << "unsupported\n";
+                        std::cerr << "Attempting to use texture_format = GL_RGB\n";
+                        texture_format = GL_RGB;
+                }
+
+                // create the texture from the image data
+                glTexImage3D(GL_TEXTURE_3D, 0, texture_format, image_width, image_height,
+                        static_cast<GLsizei>(n_image), 0, texture_format, GL_UNSIGNED_BYTE, NULL);
+
+                // TODO move these parameters into member functions
+                // make the texture wrap in s,t directions and clamp to edge in r
+                glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+                // set min and mag filters
+                glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+                //glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                //glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            }
+
+            // load image data from current image into texture
+            glTexSubImage3D(
+                    GL_TEXTURE_3D, 0,
+                    0, 0, i,
+                    image_width, image_height, 1,
+                    texture_format, GL_UNSIGNED_BYTE, image_data);
+        }
+        else
+        {
+            std::cerr << "Texture3D::texture_from_file - stbi_load failed\n";
+            //exit(1);
+        }
+
+        // free the image memory
+        stbi_image_free(image_data);
+    }
+
+    // generate mipmaps
+    glGenerateMipmap(GL_TEXTURE_3D);
+
+    // unbind the texture
+    glBindTexture(GL_TEXTURE_3D, 0);
 
     // return the texture id
     return id;
